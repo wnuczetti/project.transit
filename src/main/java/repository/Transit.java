dd942428971ff0b9651600955e73deaaa756e2e4
wnuczetti <wnuczetti@gmail.com>
2018-05-12 12:12:31 +0200

    Initial commit

---
+++ src/main/java/repository/Transit.java
@@ -0,0 +1,46 @@
+package repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
+public class Transit {
+
+    private String source;
+    private String destination;
+    private int price;
+    private Date date;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
